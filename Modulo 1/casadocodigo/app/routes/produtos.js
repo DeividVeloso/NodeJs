7d71803cc70885d36e4b22e31fbbed3bb42dcc30
SoulReaperNZ <veloso.deivid@gmail.com>
2016-10-22 10:45:57 -0200

    Add ProdutosBanco

--- Modulo 1/casadocodigo/app/routes/produtos.js
+++ Modulo 1/casadocodigo/app/routes/produtos.js
@@ -6,9 +6,9 @@ module.exports = function(app){
  app.get('/produtos',function(req, res){
         
         var connection = app.infra.connectionFactory(); 
-
+        var produtosBanco = app.infra.produtosBanco;
         //executa a query no banco e volta uma função de callback(primeiro arg erro, segundo arg resultado)
-        connection.query('select * from livros', function(err, results){
+        produtosBanco.lista(connection,function(err, results){
             //res.send(results);
             //Rederizo a pagina e envio o result para o meu html, crio um objeto Java script chamdo lista recebendo results 
             res.render('produtos/lista',{lista:results});
